23b7fff1ae12c22b3a2d3828405d648174e801df
jan <jan@764ca71e-d930-0410-8018-e5b3883f0272>
2007-05-20 20:47:25 +0000

    renamed file. fuck me.
    git-svn-id: http://svn.headflash.com/svn/janvarious/todo_federator/trunk@5 764ca71e-d930-0410-8018-e5b3883f0272

---
+++ lib/jifty_api.rb
@@ -0,0 +1,29 @@
+class JiftyApi
+  
+  @client = nil
+  
+ 
+ def initialize(proxy)
+   @conf = conf
+   @client = HTTPAccess2::Client.new(proxy,"RubyHiveminderApi", "")
+ end
+ 
+ def call(klass, params)
+   # TODO: Refactor into a multi_call which allows for more than one function call per request
+   moniker = 'fnord'
+   jifty_params = { "J:A-#{moniker}" => klass}
+   params.each do |k,v|
+     jifty_params["J:A:F-#{k.to_s}-#{moniker}"] = v.to_s
+   end
+   res = @client.post(@conf['site'] + "/__jifty/webservices/yaml",HTTP::Message.escape_query(jifty_params))
+   if res.status = HTTP::Status::OK
+     moniker_result = YAML.load(res.body.content)[moniker]
+     return moniker_result.nil? ? nil : moniker_result.value
+   else
+     raise "Bad Status from Hiveminder: #{res.status}"
+   end
+ end
+ 
+ 
+ 
+end
