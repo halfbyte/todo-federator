86be0fb77acc27ba91867545efb07b50ac756c4e
jan <jan@764ca71e-d930-0410-8018-e5b3883f0272>
2007-05-20 20:47:04 +0000

    need to rename
    git-svn-id: http://svn.headflash.com/svn/janvarious/todo_federator/trunk@4 764ca71e-d930-0410-8018-e5b3883f0272

--- lib/JiftyApi.rb
+++ lib/JiftyApi.rb
@@ -11,14 +11,13 @@ class JiftyApi
  def call(klass, params)
    # TODO: Refactor into a multi_call which allows for more than one function call per request
    moniker = 'fnord'
-   jifty_params = { "J:A-#{@moniker}" => klass}
+   jifty_params = { "J:A-#{moniker}" => klass}
    params.each do |k,v|
-     jifty_params["J:A:F-#{k.to_s}-#{@moniker}"] = v.to_s
+     jifty_params["J:A:F-#{k.to_s}-#{moniker}"] = v.to_s
    end
-   #puts jifty_params.inspect
    res = @client.post(@conf['site'] + "/__jifty/webservices/yaml",HTTP::Message.escape_query(jifty_params))
    if res.status = HTTP::Status::OK
-     moniker_result = YAML.load(res.body.content)[@moniker]
+     moniker_result = YAML.load(res.body.content)[moniker]
      return moniker_result.nil? ? nil : moniker_result.value
    else
      raise "Bad Status from Hiveminder: #{res.status}"
